fb709dec7453936e03edf6d0a4d1fcf64521ceac
malbsubo28 <amsbox182@gmail.com>
2022-12-08 22:13:28 +0700

    App.js

--- App.js
+++ App.js
@@ -1,117 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+import React from 'react'
+import{
   Text,
-  useColorScheme,
   View,
-} from 'react-native';
+  ScrollView,
+  StyleSheet,
+  useColorScheme
+} from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
+const AppHeader = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+  return(
+    <View style={{height: 100, backgroundColor: isDarkMode ? '#000505' : '#eee8d5'}}>
+      <Text style={{fontSize: 25, fontWidth: '600', paddingTop: 5}}>DodList</Text>
+      <Text>Daily todo-list</Text>
     </View>
   );
-};
+}
 
-const App: () => Node = () => {
+const Date = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  return(
+    <View>
+      <Text style={{fontSize: 18}}>Date: </Text>
+    </View>
+  );
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const Task = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return(
+    <View style={{height: 50, backgroundColor: isDarkMode ? '#073642' : '#93aeae'}}>
+      <Text style={{fontSize: 18}}>Task1</Text>
+      <Date/>
+    </View>
+  );
+}
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundColorMode = {
+    backgroundColor: isDarkMode ? '#000000' : '#eee8d5',
   };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+  return(
+    <ScrollView 
+      contentInsetAdjustmentBehavior="automatic"
+      style={backgroundColorMode}>
+      <View>
+        <AppHeader/>
+        <Task/>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
 });
 
 export default App;
