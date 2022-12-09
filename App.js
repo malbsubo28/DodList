fda0d1f6a5ac9ff1e3bc2728fa08751d3ecfa0eb
malbsubo28 <amsbox182@gmail.com>
2022-12-10 01:08:02 +0700

    App.js

--- App.js
+++ App.js
@@ -11,27 +11,43 @@ import Task from './src/components/tasks'
 const AppHeader = () => {
   const isDarkMode = useColorScheme() === 'dark';
   return(
-    <View style={styles.appHeaderBox}>
-      <View style={{
-        backgroundColor: '#002b36',
-        marginTop: 5,
-        width: 40,
-        height: 40,
-        borderRadius: 50,
-      }}></View>
-      <Text style={{
-        fontSize: 20,
-        marginLeft: 10,
-        fontWeight: '750',
-        color: isDarkMode ? '#eee8d5' : '#000000',
-      }}>DodList</Text>
-      <View style={{
-        backgroundColor: '#002b36',
-        marginTop: 5,
-        width: 40,
-        height: 40,
-        borderRadius: 50,
-      }}></View>
+    <View>
+      <View style={styles.appHeaderBox}>
+        <View style={{
+          backgroundColor: '#002b36',
+          marginTop: 5,
+          width: 40,
+          height: 40,
+          borderRadius: 50,
+        }}></View>
+        <Text style={{
+          fontSize: 20,
+          marginLeft: 10,
+          fontWeight: '750',
+          color: isDarkMode ? '#eee8d5' : '#000000',
+        }}>DodList</Text>
+        <View style={{
+          backgroundColor: '#002b36',
+          marginTop: 5,
+          width: 40,
+          height: 40,
+          borderRadius: 50,
+        }}></View>
+      </View>
+      <View style={styles.appHeaderInfo}>
+        <View style={{position: 'relative'}}>
+          <Text style={{
+            fontSize: 15,
+            fontWeight: '600',
+          }}>Today tasks </Text>
+        </View>
+        <View style={styles.notifyWrapper}>
+          <Text style={styles.taskNotify}>10</Text>
+        </View>
+        <View style={styles.addTaskIcon}>
+          <Text style={styles.taskNotify}>10</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -47,7 +63,20 @@ const App = () => {
       style={backgroundColorMode}>
       <View>
         <AppHeader/>
-        <View style={{marginTop: 20}}>
+        <View>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
+          <Task/>
           <Task/>
           <Task/>
         </View>
@@ -63,6 +92,34 @@ const styles = StyleSheet.create({
     padding: 5,
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  appHeaderInfo:{
+    backgroundColor: '#93a1a1',
+    height: 30,
+    padding: 5,
+    flexDirection: 'row',
+    position: 'relative'
+  },
+  taskNotify: {
+    fontSize: 13,
+    fontWeight: '700'
+  },
+  notifyWrapper: {
+    backgroundColor: '#3258a0',
+    width: 25,
+    height: 25,
+    borderRadius: 50,
+    alignItems: 'center'
+  },
+  addTaskIcon:{
+    backgroundColor: '#3258a0',
+    width: 25,
+    height: 25,
+    borderRadius: 50,
+    alignItems: 'center',
+    position: 'relative',
+    top: 0,
+    right: 0
   }
 })
 
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -28,17 +28,20 @@ const Task = () => {
 
 const styles = StyleSheet.create({
   taskBox:{
-    backgroundColor: '#073642',
+    backgroundColor: '#22303c',
     margin: 5,
     borderRadius: 10,
+    position: 'relative'
   },
   taskText:{
+    color : '#eee8d5',
     fontSize: 18,
     paddingLeft: 5,
     paddingRight: 5,
     flexWrap: 'wrap'
   },
   taskDate:{
+    color : '#eee8d5',
     fontSize: 15,
     paddingLeft: 5,
     paddingRight: 5,
