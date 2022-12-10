2e13b353e1f4afcf1cecef869721eb4de30108d6
malbsubo28 <amsbox182@gmail.com>
2022-12-11 03:35:20 +0700

    CheckBox and Delete Icon

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import{
   ScrollView,
   StyleSheet,
   SafeAreaView,
+  StatusBar,
   useColorScheme
 } from 'react-native';
-import Task from './components/tasks';
+import Task from './components/Task';
 import appImage from './assets/images/appImage.png';
 
 const AppHeader = ({jobs}) => {
@@ -74,8 +75,15 @@ const AppHeader = ({jobs}) => {
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? '#000505' : '#ececed'
+  };
   return(
     <SafeAreaView style={{backgroundColor: isDarkMode ? '#000000' : '#ffffff', flex: 1}}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
       <AppHeader jobs={15}/>
       <ScrollView 
         contentInsetAdjustmentBehavior='automatic'
@@ -88,11 +96,11 @@ const App = () => {
           <Task time="12.00">Meeting ...</Task>
           <Task time="15.00">Nyicil project</Task>
           <Task time="20.00">Workout malam 1 Jam</Task>
-          <Task time="20.00">Task 6</Task>
+          <Task time="20.00">Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</Task>
           <Task time="20.00">Task 7</Task>
           <Task time="20.00">Task 8</Task>
           <Task time="20.00">Task 9</Task>
-          <Task time="20.00">Task 10</Task>
+          <Task time="20.00">Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Task>
           <Task time="20.00">Task 11</Task>
           <Task time="20.00">Task 12</Task>
           <Task time="20.00">Task 13</Task>
--- src/components/Task.js
+++ src/components/Task.js
@@ -8,7 +8,8 @@ import {
 } from 'react-native';
 import trashWhite from '../assets/icons/trashWhite.png';
 import trashBlack from '../assets/icons/trashBlack.png';
-
+import checkWhite from '../assets/icons/checkWhite.png';
+import checkBlack from '../assets/icons/checkBlack.png';
 
 const Task = ({children, time}) => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,27 +20,40 @@ const Task = ({children, time}) => {
       margin: 7,
       borderRadius: 10,
       flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      position: 'relative'
     }}>
       <View>
+        <Image source={isDarkMode ? checkBlack : checkWhite} style={{
+          width:26,
+          height:26,
+          position: 'absolute',
+          left: 0,
+          top: 2
+        }}></Image>
         <Text style={{
           color : isDarkMode ? '#eee8d5' : '#000000',
           fontSize: 17,
-          flexWrap: 'wrap'
+          flexWrap: 'wrap',
+          marginRight: 35,
+          marginLeft: 35
         }}>{children}</Text>
         <Text style={{
           color : '#93a1a1',
           fontSize: 13,
-          fontWeight: '350'
+          fontWeight: '350',
+          marginLeft: 35
         }}>{time}</Text>
       </View>
       <View style={{
         backgroundColor: isDarkMode ? '#000505' : '#ececed',
       }}>
         <Image source={isDarkMode ? trashBlack : trashWhite} style={{
-          width:25,
-          height:25,
+          width:23,
+          height:23,
+          position: 'absolute',
+          right: 0,
+          top: 3
         }}></Image>
       </View>
     </View>
